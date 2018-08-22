669eb991ded896d84a8e2f48e9383c16348443af
alvintangzc <4008321@qq.com>
2018-08-22 17:57:34 +0800

    not start with server mode

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -58,9 +58,9 @@
     (exec-path-from-shell-initialize)))
 
 ;; Start server
-(use-package server
-  :ensure nil
-  :hook (after-init . server-mode))
+;(use-package server
+;  :ensure nil
+;  :hook (after-init . server-mode))
 
 ;; History
 (use-package saveplace
